8cc6a99197a5c1cde1abcc8ed7b6ed626def9b6a
Neha Sharma <sharma.neha.ns729@gmail.com>
2022-02-09 13:59:01 +0545

    CRUD operation in app

--- controller/index.js
+++ controller/index.js
@@ -1,4 +1,57 @@
+const List = require('../model/Index')
 
-exports.create = function(req,res,next){
-res.render('../views/index');
+
+exports.getPost = function(req,res,next){
+    List.find().exec((err,data)=>{
+        if(err){
+            throw err;
+        }
+        if(data){
+            return res.render('index',{data});
+        }
+    })
 }
+
+exports.addPost = async(req,res,next)=>{
+    const text = new List({description:req.body.text})
+try{
+const result = await text.save();
+if(result){
+    res.redirect('/')
+}
+}
+catch(err){
+throw err;
+}
+}
+exports.editPost = (req,res,next)=>{
+    let id = req.params._id;
+    return res.render('editPost',{id});
+}
+
+exports.updatePost = (req,res,next)=>{
+List.findByIdAndUpdate(req.body.id, 
+    {description:req.body.editText}, function(err, data) {
+        if(err){
+            console.log(err);
+        }
+        if(data){
+            //console.log("Data updated!");
+            res.redirect('/');
+        }
+    });   
+}
+
+exports.deletePost = (req,res,next)=>{
+    List.findByIdAndDelete((req.params._id), (err,data)=>{
+        if(err){
+            console.log(err);
+        }
+        if(data){
+            console.log("deleted Successfully");
+            res.redirect('/');
+        }
+    })
+   
+}
+
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
+const { route } = require('../app');
 const app = require('../app');
 var router = express.Router();
 let indexController = require('../controller/index')
 
 /* GET home page. */
 
-router.get('/', indexController.create);
+router.get('/', indexController.getPost);
+router.post('/addPost', indexController.addPost);
+router.get("/edit/:_id", indexController.editPost);
+router.post('/updatePost',indexController.updatePost);
+router.get('/delete/:_id', indexController.deletePost);
 
 module.exports = router;
